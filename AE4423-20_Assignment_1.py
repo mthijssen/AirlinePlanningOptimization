721c42e6f1e98758144fbe38e924ef7a37dd1356
silhavv <165905524+silhavv@users.noreply.github.com>
2025-11-26 15:25:29 +0100

    Create AE4423-20_Assignment_1.py
    Python File Assignment 1

---
+++ AE4423-20_Assignment_1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 26 14:37:41 2025
+
+@author: silha
+"""
+import numpy as np
+import pandas as pd
+#import mathplotlib.pyplot as plt
+
+data = pd.read_excel('C:\AllFiles\DemandGroup12.xlsx', index_col=1)
+
+data.head()
+
+
