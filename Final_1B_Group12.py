61e5ed673b32fd5fb6b30c84f2834ab73293c532
Mats Thijssen <m.a.thijssen@student.tudelft.nl>
2025-12-20 19:15:44 +0100

    Bug fix

--- Final_1B_Group12.py
+++ Final_1B_Group12.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from gurobipy import Model, GRB, quicksum
 
-from AE4423_20_Assignment_1A import build_df, build_OD_long
+from Final_1A_Group12 import build_df, build_OD_long
 
 df, demand_data = build_df()
 OD_long, model, k, b1, b2, b3 = build_OD_long()
